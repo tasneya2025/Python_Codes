3eaa7654d4097c5f843d5901b9bef323a5738b4d
tasneya2025 <repha5505urbi@gmail.com>
2026-01-28 13:32:27 +0600

    Initial commit

---
+++ firstProgram.py
@@ -0,0 +1,58 @@
+#print('hello world')
+"""
+name = "urbi"
+print(type(name))
+age =25
+print(type(age))
+age2=32.5
+print(type(age2))
+bolean = True 
+print(type(bolean))
+a = None
+print(type(a))
+
+
+
+#type casting
+a = 32
+b = 23.5
+c = "50"
+sum = a+b
+print(sum) 
+
+# sum2 = b+c
+# print(sum2) #gives an error
+
+sum2 = int(c)+b
+print(sum2)
+
+#input taking 
+name=input("Enter your name: ")
+print("Your name is: ",name)
+age =int(input("Enter your age "))
+print("You are ",age ," years old")
+
+#square of an area
+
+a = float(input("Enter the size of the square "))
+area = a**2
+print("Square area is: ",area)
+
+"""
+
+#average of 2 numbers
+p=25.60
+q=24.40
+
+sum=p+q
+avg = sum/2
+print("avg of 2 number is ",avg)
+
+a = False
+b = True
+print(a and b)
+print(a or b)
+print (not b)
+
+
+
