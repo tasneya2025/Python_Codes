62dbe4eed1d6f26e77aa050f22311e42529b0e16
tasneya2025 <repha5505urbi@gmail.com>
2026-02-02 22:45:02 +0600

    while loop learning

---
+++ whileLoop.py
@@ -0,0 +1,43 @@
+# i = 1
+# while i<=100 :
+#     print(i)
+#     i+=1
+
+
+
+
+# i = 100
+# while i>=1 :
+#     print(i)
+#     i-=1
+
+
+
+
+# n = int(input("Enter number: "))
+# i = 1
+# while i<=10 :
+#     print(n*i)
+#     i+=1
+
+
+# nums = [1,4,7,9,11,13,16,19,24,33,39,45,59]
+# i = 0
+# while i< len(nums):
+#     print(nums[i])
+#     i+=1
+
+
+
+nums = (1,4,7,9,11,13,16,19,24,33,39,45,59)
+x = int(input("Enter the number you're searching: "))
+i = 0
+while i<len(nums):
+    if(x==nums[i]):
+        print("Number found! at index ",i)
+    i+=1    
+
+
+
+
+     
