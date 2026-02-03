28d7681d01e162b5155496b381c7c5ed33145a1e
tasneya2025 <repha5505urbi@gmail.com>
2026-02-03 14:26:12 +0600

    break and continue

--- whileLoop.py
+++ whileLoop.py
@@ -29,14 +29,24 @@
 
 
 
-nums = (1,4,7,9,11,13,16,19,24,33,39,45,59)
-x = int(input("Enter the number you're searching: "))
-i = 0
-while i<len(nums):
-    if(x==nums[i]):
-        print("Number found! at index ",i)
-    i+=1    
+# nums = (1,4,7,9,11,13,16,19,24,33,39,45,59)
+# x = int(input("Enter the number you're searching: "))
+# i = 0
+# while i<len(nums):
+#     if(x==nums[i]):
+#         print("Number found! at index ",i)
+#     i+=1    
 
+
+
+#break and continue
+i=0
+while i<=5:
+    if(i==3):
+        i+=1
+        continue    
+    print(i)
+    i+=1    
 
 
 
