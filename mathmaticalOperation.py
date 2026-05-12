174c959f9caf049e35b15ca466ac0037884cafab
tasneya2025 <repha5505urbi@gmail.com>
2026-05-12 13:09:30 +0600

    mathmatical operation is done

---
+++ mathmaticalOperation.py
@@ -0,0 +1,11 @@
+print(2+3)
+print(33-12)
+print(2*2)
+print(6/2)
+print(type(6/2))
+print(6//2) #it gives int value of the division
+print(type(6//2))
+
+#PEMDAS (LEFT TO RIGHT)
+print(3*3+3/3-3) #first multiplication & division priority and then left to right priority 
+
